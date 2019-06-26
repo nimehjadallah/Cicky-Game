f7522d9d28df913c354f1e069986747905d01c24
Nimeh Jadallah <nimeh_abueid@hotmail.com>
2019-06-26 15:05:50 -0400

    added a readme and styling

--- clicky-game/src/components/EmojiCard/EmojiCard.js
+++ clicky-game/src/components/EmojiCard/EmojiCard.js
@@ -3,10 +3,12 @@ import "./EmojiCard.css";
 
 //pass the image into each card so all 12 are rendered
 const EmojiCard = props => (
+    <div className = "wrapper">
     <div className = "card" onClick={props.imageClick}>
         <div className = "imgContainer">
             <img alt={props.image.replace(".jpg","")} src={require("../../images/" + props.image)}/>
         </div>
+    </div>
     </div>
 );
 
