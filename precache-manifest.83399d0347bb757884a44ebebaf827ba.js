82f0901dc0a4a1380b38acf2a79f25b24bdcb419
Nimeh Jadallah <nimeh_abueid@hotmail.com>
2019-06-26 13:24:29 -0400

    Updates

---
+++ precache-manifest.83399d0347bb757884a44ebebaf827ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3237f692c6c2e4fa697d7b3f7aa9a1a",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "cc20365950d92a52e7f8",
+    "url": "/Clicky-Game/static/css/main.eafb7e05.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Clicky-Game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "cc20365950d92a52e7f8",
+    "url": "/Clicky-Game/static/js/main.09dcd460.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
